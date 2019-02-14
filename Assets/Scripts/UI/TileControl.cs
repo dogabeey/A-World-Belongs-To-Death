c4d409a663164ab3c379cfc8c6611683e20965fd
dogabeey@gmail.com <dogabeey@gmail.com>
2019-02-15 00:57:44 +0300

    major changes like larger world map, tilemap optimizations and few code corrections

--- Assets/Scripts/UI/TileControl.cs
+++ Assets/Scripts/UI/TileControl.cs
@@ -7,11 +7,13 @@ public class TileControl : MonoBehaviour
 {
     public Tile tile;
     public int x, y;
-    public GameObject worldModel;
+    GameObject worldModel;
+    static GameObject activeTile;
 
     private void Start()
     {
         worldModel = GameObject.FindGameObjectWithTag("world");
+        activeTile = null;
     }
 
     public List<GameObject> GetNeighbors()
@@ -25,12 +27,15 @@ public class TileControl : MonoBehaviour
         return retVal;
     }
 
-    void OnMouseUpAsButton()
+    void OnMouseDown()
     {
-        for (int i = 0; i < worldModel.GetComponentsInChildren<Outline>().Length; i++)
+        if (activeTile != null)
         {
-            worldModel.GetComponentsInChildren<Outline>()[i].eraseRenderer = true;
+            Debug.Log("Deactivated " + gameObject.name);
+            activeTile.GetComponent<Outline>().eraseRenderer = true;
         }
+        Debug.Log("Mouse clicked on" + gameObject.name);
         gameObject.GetComponent<Outline>().eraseRenderer = false;
+        activeTile = gameObject;
     }
 }
